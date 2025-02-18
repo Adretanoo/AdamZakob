510788e9a327712dfcd8428d8f4669272d6d1cd0
Adretanoo <adriantegzza@gmail.com>
2025-02-19 01:10:04 +0200

    add block portfolio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+window.addEventListener("load", function () {
+  const preloader = document.getElementById("preloader");
+  const textElement = document.querySelector(".text");
+  const text = "Adam Zakob";
+  let index = 0;
+
+  function typeText() {
+      if (index < text.length) {
+          textElement.innerHTML += text[index];
+          index++;
+          setTimeout(typeText, 150);
+      } else {
+          setTimeout(() => {
+              preloader.classList.add("hide");
+              setTimeout(() => {
+                  preloader.style.display = "none";
+              }, 500);
+          }, 1000);
+      }
+  }
+
+  setTimeout(typeText, 500);
+});
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const scrollLinks = document.querySelectorAll('[data-scroll]');
